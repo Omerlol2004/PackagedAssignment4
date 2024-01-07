a410e0aa8ed2d37d76e4c0156117f91bef77fc5b
Omerlol2004 <152614143+Omerlol2004@users.noreply.github.com>
2024-01-07 14:21:42 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PackagedAssignment4',  # Replace with your desired package name
+    version='0.1.0',  # Update this version number as needed (follow semantic versioning)
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here if any
+    ],
+)
